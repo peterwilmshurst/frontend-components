72456e06deae915d2680d06af66009285f4588b7
Peter Wilmshurst <peter.wilmshurst@domtar.com>
2019-11-29 14:25:14 +0000

    update navigation etc

--- src/js/main.js
+++ src/js/main.js
@@ -17,37 +17,41 @@ hamburger.addEventListener('click', function () {
     body.classList.toggle("nav-open")
 });
 
-// Vanilla JavaScript Scroll to Anchor
-// @ https://perishablepress.com/vanilla-javascript-scroll-anchor/
+// // Vanilla JavaScript Scroll to Anchor
+// // @ https://perishablepress.com/vanilla-javascript-scroll-anchor/
 
-(function () {
-    scrollTo();
-})();
+// (function () {
+//     scrollTo();
+// })();
 
-function scrollTo() {
-    const links = document.querySelectorAll('.scroll');
-    links.forEach(each => (each.onclick = scrollAnchors));
-}
+// function scrollTo() {
+//     const links = document.querySelectorAll('.scroll');
+//     links.forEach(each => (each.onclick = scrollAnchors));
+// }
 
-function scrollAnchors(e, respond = null) {
-    const distanceToTop = el => Math.floor(el.getBoundingClientRect().top);
-    e.preventDefault();
-    var targetID = (respond) ? respond.getAttribute('href') : this.getAttribute('href');
-    const targetAnchor = document.querySelector(targetID);
-    if (!targetAnchor) return;
-    const originalTop = distanceToTop(targetAnchor);
-    window.scrollBy({
-        top: originalTop,
-        left: 0,
-        behavior: 'smooth'
-    });
-    const checkIfDone = setInterval(function () {
-        const atBottom = window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 2;
-        if (distanceToTop(targetAnchor) === 0 || atBottom) {
-            targetAnchor.tabIndex = '-1';
-            targetAnchor.focus();
-            window.history.pushState('', '', targetID);
-            clearInterval(checkIfDone);
-        }
-    }, 100);
-}
+// function scrollAnchors(e, respond = null) {
+//     const distanceToTop = el => Math.floor(el.getBoundingClientRect().top);
+//     e.preventDefault();
+//     var targetID = (respond) ? respond.getAttribute('href') : this.getAttribute('href');
+//     const targetAnchor = document.querySelector(targetID);
+//     if (!targetAnchor) return;
+//     const originalTop = distanceToTop(targetAnchor);
+//     window.scrollBy({
+//         top: originalTop,
+//         left: 0,
+//         behavior: 'smooth'
+//     });
+//     const checkIfDone = setInterval(function () {
+//         const atBottom = window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 2;
+//         if (distanceToTop(targetAnchor) === 0 || atBottom) {
+//             targetAnchor.tabIndex = '-1';
+//             targetAnchor.focus();
+//             window.history.pushState('', '', targetID);
+//             clearInterval(checkIfDone);
+//         }
+//     }, 100);
+// }
+
+$("#menu li").find("a").each(function () {
+    new RegExp($(this).attr("href")).test(document.location.href) && ($(this).addClass("active"))
+})
